815f8f266853153640ba086a91fb9bc8f1f66aa2
Travis Coots <57024574+tcoots88@users.noreply.github.com>
2019-11-03 19:00:42 -0800

    Create dynamic display.js

---
+++ dynamic display.js
@@ -0,0 +1,8 @@
+var userAnswer = confirm('Did you start Russel Wilson?'); // returns true or false
+
+if (userAnswer == true) {
+    alert("Well Played")
+}
+else {
+    alert("Well, hopefully you didn't start Brissett")
+}
